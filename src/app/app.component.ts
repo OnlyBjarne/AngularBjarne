3e14ce5314b6dacb37d7efb1bd2f1ef3d9f39323
OnlyBjarne <bjakke112@gmail.com>
2017-03-14 16:47:14 +0100

    testing buttons and clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  private header:String = "Header";
+  private counter:number = 1;
+
+  private toggle:Boolean = true;
+
+  private countUp(){
+    this.counter+=1;
+  }
+  private countDown(){
+    this.counter-=1;
+  }
+
+
+  private trigger() {
+    this.toggle = !this.toggle;
+  }
+  
+}
